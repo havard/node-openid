7ed78a8fb2071aeac9d2442af7794d278f982761
Håvard Stranden <havard.stranden@gmail.com>
2011-11-15 16:13:15 +0100

    Set proper Content-Type in sample responses

--- sample.js
+++ sample.js
@@ -70,12 +70,12 @@ var server = require('http').createServer(
           {
             if(error)
             {
-              res.writeHead(200);
-              res.end('Authentication failed: ' + error);
+              res.writeHead(200, { 'Content-Type' : 'text/plain; charset=utf-8' });
+              res.end('Authentication failed: ' + error.message);
             }
             else if (!authUrl)
             {
-              res.writeHead(200);
+              res.writeHead(200, { 'Content-Type' : 'text/plain; charset=utf-8' });
               res.end('Authentication failed');
             }
             else
@@ -91,7 +91,7 @@ var server = require('http').createServer(
           // NOTE: Passing just the URL is also possible
           relyingParty.verifyAssertion(req, function(error, result)
           {
-            res.writeHead(200);
+            res.writeHead(200, { 'Content-Type' : 'text/plain; charset=utf-8' });
 
             if(error)
             {
@@ -112,7 +112,7 @@ var server = require('http').createServer(
         else
         {
             // Deliver an OpenID form on all other URLs
-            res.writeHead(200);
+            res.writeHead(200, { 'Content-Type' : 'text/html; charset=utf-8' });
             res.end('<!DOCTYPE html><html><body>'
                 + '<form method="get" action="/authenticate">'
                 + '<p>Login using OpenID</p>'
