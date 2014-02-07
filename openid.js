954b7348c02dba3af53e40275101562be1ce392d
Chris Flook <glasseyes42@googlemail.com>
2014-02-07 15:55:14 +0000

    Pass URI-keyed values in AX response

--- openid.js
+++ openid.js
@@ -1498,6 +1498,7 @@ openid.AttributeExchange.prototype.fillResult = function(params, result)
     if (aliases[ns] in values)
     {
       result[aliases[ns]] = values[aliases[ns]];
+      result[ns] = values[aliases[ns]];
     }
   }
 }
