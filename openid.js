da443ee8844f687b6d92ccfad805eb53130cd149
Håvard Stranden <havard.stranden@gmail.com>
2016-09-16 00:38:08 +0200

    Fix issues with using Buffer.from and binary encoding in certain older Node versions.

--- openid.js
+++ openid.js
@@ -91,7 +91,26 @@ var _isDef = function(e)
   return e !== undefined;
 }
 
-var _buffer = typeof(Buffer.from) === 'function' ? Buffer.from : function(str, enc) { return new Buffer(str, enc); }; 
+// Find the most up-to-date and usable way to create buffers
+var _buffer = null;
+if (typeof(Buffer.from) === 'function') {
+  // Some older Node versions throw an exception when 
+  // buffers with binary encoding are created using the
+  // from function, so if that happens we have to resort
+  // to constructor based creation.
+  try {
+    Buffer.from('openid', 'binary');
+    _buffer = Buffer.from;
+  }
+  catch(_) {
+  }
+}
+if (_buffer === null) {
+  // Either the Node version is too old to have a Buffer.from,
+  // or the Buffer.from call failed with binary encoding.
+  // Either way, use the (deprecated from node v6) constructor.
+  _buffer = function(str, enc) { return new Buffer(str, enc); };
+} 
 
 var _base64encode = function(str) {
   return _buffer(str, 'binary').toString('base64');
